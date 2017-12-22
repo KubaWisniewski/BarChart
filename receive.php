ef85e475b3092dcb664f20b03be2bfd5969aeeef
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2017-12-22 02:19:09 +0100

    Second refactor

--- receive.php
+++ receive.php
@@ -8,20 +8,20 @@ $dane_testowe=$_GET['dane_testowe'];
 $test=false;
 
 if($dane_testowe==1){
-for($i=0;$i<6;$i++){
+	for($i=0;$i<6;$i++){
 		$tab[$i]=rand(1,60);
-	}
-$szer_test=500;
-$wys_test=500;
-$id_kolor_test=10;
-$test=true;
-wykres($tab,$szer_test,$wys_test,$id_kolor_test,$test);
+		} 
+	$szer_test=500;
+	$wys_test=500;
+	$id_kolor_test=10;
+	$test=true;
+	wykres($tab,$szer_test,$wys_test,$id_kolor_test,$test);
 }
 else{
-for($i=0;$i<count($tab2);$i++){
-	$tab=array_push_assoc($tab,$tab1[$i],$tab2[$i]);
-	}
-wykres($tab,$szer,$wys,$id_kolor,$test);
+	for($i=0;$i<count($tab2);$i++){
+		$tab=array_push_assoc($tab,$tab1[$i],$tab2[$i]);
+		}
+	wykres($tab,$szer,$wys,$id_kolor,$test);
 }
 
 function array_push_assoc($array, $key, $value){
@@ -67,11 +67,11 @@ function wykres($tab,$szerokosc,$wysokosc,$kolor_wykresu,$test){
 	$ilosc_elementow=count($tab);
 	$suma=0;
 	foreach($tab as $klucz=>$wart){
-	$suma+=$wart;
+		$suma+=$wart;
 	}
 	//Wyliczanie wartosci procentowych podanych danych
 	foreach($tab as $klucz=>$wart){
-	$procenty[$klucz]=($wart/$suma)*100;
+		$procenty[$klucz]=($wart/$suma)*100;
 	}
 	
 	$max=max($tab);	
@@ -93,59 +93,59 @@ function wykres($tab,$szerokosc,$wysokosc,$kolor_wykresu,$test){
 	$skok_x=($wysokosc_robocza)/10;
 	$skok=$skok_x;
 	$i=1;
+	
 	//Rysowanie legendy i skali 
 	for ($j=0; $j<10; $j++) {
-            imageline($im,$miejsce_na_skale, $wysokosc_robocza-$skok, $szerokosc, $wysokosc_robocza-$skok, $kolor[13]);
-            imagestring($im,$czcionka_skala, 0, $wysokosc_robocza-$skok,($i*10)."%", $kolor[13]);
-			++$i;
-			$skok+=$skok_x;
-        }
+        imageline($im,$miejsce_na_skale, $wysokosc_robocza-$skok, $szerokosc, $wysokosc_robocza-$skok, $kolor[13]);
+        imagestring($im,$czcionka_skala, 0, $wysokosc_robocza-$skok,($i*10)."%", $kolor[13]);
+		++$i;
+		$skok+=$skok_x;
+    }
 	//Rysowanie slupkow
 	if($test==false){
-	for ($j=1; $j<=floor($odstep*0.5); $j++) {
+		for ($j=1; $j<=floor($odstep*0.5); $j++) {
 		//Ryswoanie efektu 3D
-		$x=0;$i=0;
-        foreach($tab as $klucz => $wart) {
-            $wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
-			imagefilledrectangle($im, $odlegloscOdLewejKrawedzi+$x+$j, $wysokosc_robocza-$wys_slupka-$j, $odlegloscOdLewejKrawedzi+$szer_slupka+$x+$j, $wysokosc_robocza-$j, $kolor_c[$kolor_wykresu[$i]]);
-            $x+=($odstep+$szer_slupka);       
-			$i++;
+			$x=0;$i=0;
+			foreach($tab as $klucz => $wart) {
+				$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
+				imagefilledrectangle($im, $odlegloscOdLewejKrawedzi+$x+$j, $wysokosc_robocza-$wys_slupka-$j, $odlegloscOdLewejKrawedzi+$szer_slupka+$x+$j, $wysokosc_robocza-$j, $kolor_c[$kolor_wykresu[$i]]);
+				$x+=($odstep+$szer_slupka);       
+				$i++;
+			}
 		}
-    }
 	$x=0;$i=0;
-	foreach($tab as $klucz =>$wart){
-		$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
-		imagefilledrectangle($im,$odlegloscOdLewejKrawedzi+$x,$wysokosc_robocza-$wys_slupka,$odlegloscOdLewejKrawedzi+$szer_slupka+$x,$wysokosc_robocza,$kolor[$kolor_wykresu[$i]]);
-		$x+=($odstep+$szer_slupka);
-		$i++;
+		foreach($tab as $klucz =>$wart){
+			$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
+			imagefilledrectangle($im,$odlegloscOdLewejKrawedzi+$x,$wysokosc_robocza-$wys_slupka,$odlegloscOdLewejKrawedzi+$szer_slupka+$x,$wysokosc_robocza,$kolor[$kolor_wykresu[$i]]);
+			$x+=($odstep+$szer_slupka);
+			$i++;
 		}
 	}
 	else{
 	for ($j=1; $j<=floor($odstep*0.5); $j++) {
 		//Rysowanie efektu 3D
 		$x=0;$i=0;
-        foreach($tab as $klucz => $wart) {
-            $wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
-			imagefilledrectangle($im, $odlegloscOdLewejKrawedzi+$x+$j, $wysokosc_robocza-$wys_slupka-$j, $odlegloscOdLewejKrawedzi+$szer_slupka+$x+$j, $wysokosc_robocza-$j, $kolor_c[$kolor_wykresu]);
-            $x+=($odstep+$szer_slupka);       
-			$i++;
-		}	
-    }
+			foreach($tab as $klucz => $wart) {
+				$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
+				imagefilledrectangle($im, $odlegloscOdLewejKrawedzi+$x+$j, $wysokosc_robocza-$wys_slupka-$j, $odlegloscOdLewejKrawedzi+$szer_slupka+$x+$j, $wysokosc_robocza-$j, $kolor_c[$kolor_wykresu]);
+				$x+=($odstep+$szer_slupka);       
+				$i++;
+			}	
+		}
 	$x=0;
-	foreach($tab as $klucz =>$wart){
-		$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
-		imagefilledrectangle($im,$odlegloscOdLewejKrawedzi+$x,$wysokosc_robocza-$wys_slupka,$odlegloscOdLewejKrawedzi+$szer_slupka+$x,$wysokosc_robocza,$kolor[$kolor_wykresu]);
-		$x+=($odstep+$szer_slupka);
+		foreach($tab as $klucz =>$wart){
+			$wys_slupka=($wysokosc_robocza*($procenty[$klucz]/100));
+			imagefilledrectangle($im,$odlegloscOdLewejKrawedzi+$x,$wysokosc_robocza-$wys_slupka,$odlegloscOdLewejKrawedzi+$szer_slupka+$x,$wysokosc_robocza,$kolor[$kolor_wykresu]);
+			$x+=($odstep+$szer_slupka);
 		}
 	}
 	
 	//Rysownaie podpisow do slupkow
 	$x=0;
-	foreach($tab as $klucz =>$wart){
-		
-		imagestring($im,$czcionka_podpisy,$odlegloscOdLewejKrawedzi+$x+($szer_slupka*0.5),$wysokosc_robocza+5,$klucz,$kolor[13]);	  	
-		$x+=($odstep+$szer_slupka);
-	}
+		foreach($tab as $klucz =>$wart){
+			imagestring($im,$czcionka_podpisy,$odlegloscOdLewejKrawedzi+$x+($szer_slupka*0.5),$wysokosc_robocza+5,$klucz,$kolor[13]);	  	
+			$x+=($odstep+$szer_slupka);
+		}
 	imagejpeg($im);
 	imagedestroy($im);	
 }
